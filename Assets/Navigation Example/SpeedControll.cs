d5995d2ecc97579de18744f5e286400edf4b24d2
A8nton <toskagg01@gmail.com>
2020-06-01 10:39:19 +0300

    AI animation system creating started

---
+++ Assets/Navigation Example/SpeedControll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class SpeedControll : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed;
+    private Animator _controller;
+
+    void Start() {
+        _controller = GetComponent<Animator>();
+    }
+
+    void Update() {
+        _controller.SetFloat("Speed", _speed);
+    }
+}
