53cc190fe42fea12c10696e38f82f99ac2d1baf3
A8nton <toskagg01@gmail.com>
2020-06-03 11:46:46 +0300

    AI state machine creating in progress

--- Assets/Dead Earth/Scripts/AI/AIState.cs
+++ Assets/Dead Earth/Scripts/AI/AIState.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public abstract class AIState : MonoBehaviour {
 
+    public void SetStateMachine(AIStateMachine stateMachine) {
+        _stateMachine = stateMachine;
+    }
 
+    public virtual void OnEnterState() { }
+    public virtual void OnExitState() { }
+    public virtual void OnAnimatorUpdated() { }
+    public virtual void OnAnimatorIKUpdated() { }
+    public virtual void OnTriggerEvent(AITriggerEventType eventType, Collider other) { }
+    public virtual void OnDestinationReached (bool isReached) { }
+
+    public abstract AIStateType OnUpdate();
     public abstract AIStateType GetStateType();
+
+    protected AIStateMachine _stateMachine;
 }
