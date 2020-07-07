fcc9fe5a37d08ee420a25ef5542caef66200612b
A8nton <toskagg01@gmail.com>
2020-07-07 11:12:36 +0300

    Attack animations prepared

---
+++ Assets/Dead Earth/Scripts/AI/MeleeZoneTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeZoneTrigger : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other) {
+        AIStateMachine machine = GameSceneManager.instance.GetAIStateMachine(other.GetInstanceID());
+        if (machine) {
+            machine.inMeleeRange = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        AIStateMachine machine = GameSceneManager.instance.GetAIStateMachine(other.GetInstanceID());
+        if (machine) {
+            machine.inMeleeRange = false;
+        }
+    }
+}
