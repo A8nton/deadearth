bb183dbd8bff378c4717cc52fa332012352fd79a
A8nton <toskagg01@gmail.com>
2020-07-15 10:26:42 +0300

    CameraBloodEffect implementing started

---
+++ Assets/Dead Earth/Scripts/Image Effects/CameraBloodEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBloodEffect : MonoBehaviour {
+
+    [SerializeField]
+    private Shader _shader;
+
+    private Material _material;
+
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (_shader == null)
+            return;
+        if (_material == null) {
+            _material = new Material(_shader);
+        }
+
+        if (_material == null)
+            return;
+
+        Graphics.Blit(source, destination, _material);
+    }
+}
